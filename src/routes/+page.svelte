111d1638324a08eb06d76786f69c173c9d2f6060
CyanCat000 <mouzhiyang2345@gmail.com>
2026-02-03 17:48:22 +0800

    feat: 样式调整

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -255,11 +255,11 @@
 </script>
 
 <main
-  class="h-screen w-screen bg-[#f0f0f0] text-[#1b1b1b] font-sans flex flex-col p-8 select-none"
+  class="h-screen w-screen bg-[#f5f5f5] text-[#1b1b1b] font-sans flex flex-col p-8 select-none"
   on:contextmenu|preventDefault
 >
 
-  <div class="mb-6 bg-white p-5 rounded-lg">
+  <div class="mb-6 bg-[#fcfcfc] p-5 rounded-lg">
     <label
       for="preset-select"
       class="block text-sm font-semibold text-[#1b1b1b] mb-3"
@@ -416,7 +416,7 @@
         已选择 {files.length} 个文件
       </div>
       <button
-        class="px-4 py-2 text-sm text-[#1b1b1b] bg-[#ffffff] hover:bg-[#f5f5f5] active:bg-[#eeeeee] rounded-md transition-all disabled:opacity-50"
+        class="px-4 py-2 text-sm border-1 border-[#dfdfdf] border-b-[#eaeaea] text-[#1b1b1b] bg-[#ffffff] hover:bg-[#f5f5f5] active:bg-[#eeeeee] rounded-md transition-all disabled:opacity-50"
         disabled={processing}
         on:click={clearFiles}
       >
@@ -425,7 +425,8 @@
     {/if}
 
     <button
-      class="px-8 py-2 bg-[#007aff] hover:bg-[#005fb8] active:bg-[#0052a1] text-white text-sm font-medium rounded-md active:shadow-none disabled:bg-[#cccccc] disabled:shadow-none transition-all flex items-center gap-2"
+      class="px-8 py-2 bg-[#ffffff] border-1 border-[#dfdfdf] border-b-[#eaeaea] hover:bg-[#f0f0f0] active:bg-[#dedede] text-black text-sm font-medium rounded-md active:shadow-none disabled:bg-[#cccccc] 
+       disabled:border-0 disabled:text-[#eeeeee] transition-all flex items-center gap-2"
       disabled={files.length === 0 || processing}
       on:click={processFiles}
     >
